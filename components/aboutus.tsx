c38c7aba491c8b1fbfcf44bc7a5bbb8bb134fa16
James Costner <jamesmp9957@outlook.com>
2022-08-04 21:11:41 -0500

    changed the about us section

--- components/aboutus.tsx
+++ components/aboutus.tsx
@@ -7,8 +7,8 @@ const AboutUs: React.FC = () => {
     <section className={`flex flex-col bg-white py-20 text-3xl md:text-4xl`}>
         <div className="container mx-auto px-11">
             <p className="leading-tight max-w-5xl mx-auto text-4xl  tracking-tight">
-               <strong>We will help you develop better distributed systems, faster.   </strong> 
-               Our team of experts engineers have created some of the best user experiences in some of the most popular applications worldwide
+               <strong>We will help you develop better SaaS,Distributed systems, and microservices, faster.   </strong> 
+               Our team of experts engineers have created some of the best user experiences in some of the most popular applications worldwide.
             </p>
         </div>
         <div className="container mx-auto px-11 text-center mt-28">

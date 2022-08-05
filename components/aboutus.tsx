d26a45018ee0481303975b8a0e099515522a48fc
James Costner <jamesmp9957@outlook.com>
2022-08-04 21:35:28 -0500

    fixed misspelling

--- components/aboutus.tsx
+++ components/aboutus.tsx
@@ -3,24 +3,24 @@ import React from 'react';
 
 const AboutUs: React.FC = () => {
 
-  return(
+  return (
     <section className={`flex flex-col bg-white py-20 text-3xl md:text-4xl`}>
-        <div className="container mx-auto px-11">
-            <p className="leading-tight max-w-5xl mx-auto text-4xl  tracking-tight">
-               <strong>We will help you develop better SaaS,Distributed systems, and microservices, faster.   </strong> 
-               Our team of experts engineers have created some of the best user experiences in some of the most popular applications worldwide.
-            </p>
+      <div className="container mx-auto px-11">
+        <p className="leading-tight max-w-5xl mx-auto text-4xl  tracking-tight">
+          <strong>We will help you develop better SaaS,Distributed systems, and microservices, faster.   </strong>
+          Our team of expert engineers have created some of the best user experiences in some of the most popular applications worldwide.
+        </p>
+      </div>
+      <div className="container mx-auto px-11 text-center mt-28">
+        <h2> Our Team</h2>
+        <div className="mt-2">the &ldquo;Gr8Minds&rdquo;</div>
+        <div className="mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grids-cols-5 gap-6 lg:gap-20">
+          <div>Members</div>
+          <div>Members</div>
+          <div>Members</div>
+          <div>Members</div>
         </div>
-        <div className="container mx-auto px-11 text-center mt-28">
-           <h2> Our Team</h2>
-           <div className="mt-2">the &ldquo;Gr8Minds&rdquo;</div>
-           <div className="mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grids-cols-5 gap-6 lg:gap-20">
-           <div>Members</div>
-           <div>Members</div>
-           <div>Members</div>
-           <div>Members</div>
-           </div>
-        </div>
+      </div>
     </section>
   )
 
